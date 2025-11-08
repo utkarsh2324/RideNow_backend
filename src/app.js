c232715bc7dd14ac4b94a7f91affe2876b91b8de
Sanskar Gupta <sanskargupta1404@gmail.com>
2025-11-08 23:27:12 +0530

    conflict fixed

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
 });
 
 // 2. Middlewares updated with safer limits
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "500mb" })); 
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
 app.use(cookieParser());
 
 // --- 3. Route Imports (Web AND App) ---
